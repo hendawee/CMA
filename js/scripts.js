ae953f868c8a63eeaa09c82a21d29c2c35c670a4
Ahmed Hendawee <ahmedhendawee@Ahmeds-MacBook-Air.local>
2024-02-03 22:42:49 +0300

    human-page

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,31 @@ function togglePasswordVisibility() {
       toggleIcon.classList.add('fa-eye');
   }
 }
+
+// Initialize tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+  // show all services in hr page
+document.querySelector('.show-all').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the link from triggering its default behavior
+  var dashboard = document.querySelector('.dash-items');
+  dashboard.style.maxHeight = dashboard.scrollHeight + "px";
+});
+
+
+function toggleHeart(event, element) {
+  // Prevent default action (if any)
+  event.preventDefault();
+  
+  // Toggle classes
+  if (element.classList.contains('fa-regular')) {
+    element.classList.remove('fa-regular');
+    element.classList.add('fa-solid');
+  } else {
+    element.classList.remove('fa-solid');
+    element.classList.add('fa-regular');
+  }
+}
